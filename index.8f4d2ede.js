d71ba2c889eeddaf5fa74e79ce6b81591f4bbc2e
DimaSomik <166946352+DimaSomik@users.noreply.github.com>
2024-10-21 22:03:16 +0000

    Deploying to gh-pages from @ DimaSomik/Cinemania_Group1@22fa7ec88b373c82bdf9d19622b2b1c540ac7ff8 🚀

---
+++ index.8f4d2ede.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.getElementById("modalTeam"),n=document.querySelector(".footer-click"),o=document.querySelector(".close-modal-click");n.onclick=function(){e.style.display="block"},o.onclick=function(){e.style.display="none"},window.onclick=function(n){n.target==e&&(e.style.display="none")},document.onkeydown=function(n){"Escape"===n.key&&(e.style.display="none")}}));
+//# sourceMappingURL=index.8f4d2ede.js.map
