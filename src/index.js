813122aafc1af8261f73eb3f4b5a7a80487f3c1e
Dima Somik <ghubdmytrosomik@gmail.com>
2024-10-22 12:51:47 +0200

    fixed problem

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './partials_js/api';
+import './partials_js/catalog';
+import './partials_js/footer';
+import './partials_js/header';
+import './partials_js/hero';
+import './partials_js/modal_1';
+import './partials_js/modal_2';
+import './partials_js/theme-switcher';
+import './partials_js/upcoming';
+import './partials_js/weekly_trends';
