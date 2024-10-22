f55b8add0214255bebd84b83d37162ea2b1225a3
Dima Somik <ghubdmytrosomik@gmail.com>
2024-10-22 20:40:07 +0200

    minor

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import './partials_js/api';
 import './partials_js/catalog';
 import './partials_js/footer';
 import './partials_js/header';
+import './partials_js/hero_started';
 import './partials_js/hero';
+import './partials_js/library';
 import './partials_js/modal_1';
 import './partials_js/modal_2';
 import './partials_js/theme-switcher';
 import './partials_js/upcoming';
 import './partials_js/weekly_trends';
-import './partials_js/library';
-import './partials_js/hero_started';
+
