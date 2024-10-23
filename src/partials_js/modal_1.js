42cda42fe7b55b2f359ab8247d0355e5f794b22a
Dima Somik <ghubdmytrosomik@gmail.com>
2024-10-23 20:12:54 +0200

    modal corrections

--- src/partials_js/modal_1.js
+++ src/partials_js/modal_1.js
@@ -1,11 +1,15 @@
+import { getMovieDetails } from './api';
+
 export async function openDetailsModal(movieId) {
+  const movieDetails = getMovieDetails(movieId);
+
   document.querySelector('.MovieCard').style.display = "flex";
 
-  document.querySelector('.ModalMovieTitle').innerText = movieId.title;
-  document.querySelector('.ModalMovieRating').innerText = movieId.vote_average;
-  document.querySelector('.ModalMovieDetails p:nth-of-type(3)').innerText = movieId.popularity;
-//   document.querySelector('.ModalMovieDetails p:nth-of-type(4)').innerText = movieId.genres.map(genre => genre.name).join(' ');
-  // document.querySelector('.ModalMovieDetails p:nth-of-type(6)').innerText = movieId.overview;
+  document.querySelector('.ModalMovieTitle').innerText = movieDetails.title;
+  document.querySelector('.ModalMovieRating').innerText = movieDetails.vote_average;
+  document.querySelector('.ModalMovieDetails p:nth-of-type(3)').innerText = movieDetails.popularity;
+//   document.querySelector('.ModalMovieDetails p:nth-of-type(4)').innerText = movieDetails.genres.map(genre => genre.name).join(' ');
+  // document.querySelector('.ModalMovieDetails p:nth-of-type(6)').innerText = movieDetails.overview;
   document.getElementById('detailsModal').style.display = 'flex'; // Otwórz modal
 };
 
