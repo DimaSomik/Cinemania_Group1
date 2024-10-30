1550d49bab5d10f8ef437465051d475773bdbf44
Dima Somik <ghubdmytrosomik@gmail.com>
2024-10-30 20:23:49 +0100

    fixes

--- src/partials_js/weekly_trends.js
+++ src/partials_js/weekly_trends.js
@@ -1,6 +1,6 @@
 import { getGenres } from './api';
 import { getPopularMoviesWeek } from './api';
-// import { showModal } from './modal_1';
+import { showModal } from './modal_1';
 
 let page = 1;
 let currentMovieIndex = 0; // Index aktualnie wyświetlanego filmu
@@ -77,6 +77,7 @@ function displayMovies() {
 
       moviesContainer.appendChild(movieCard);
       currentMovieIndex++; // Zwiększ indeks filmu
+      addCardModals(movie);
     } else {
       // Jeśli filmy się skończą, załaduj kolejną stronę
       page++;
@@ -87,85 +88,85 @@ function displayMovies() {
 }
 
 // Obsługa przycisku "Załaduj więcej"
-loadMoreButton.addEventListener('click', () => {
-  // page++;
-  // getMovies(page);
+// loadMoreButton.addEventListener('click', () => {
+//   // page++;
+//   // getMovies(page);
 
-  openCatalogPage();
-});
+//   openCatalogPage();
+// });
 
 // Najpierw pobierz listę gatunków, a potem filmy
 getMovies(page);
 
-let moviesLoaded = false;
+// let moviesLoaded = false;
 
-function loadMoreMovies() {
-  if (!moviesLoaded) {
-    // Symulacja ładowania filmów (w rzeczywistości tu może być API)
-    const newMovies = [
-      { title: 'Movie 4', year: 2023 },
-      { title: 'Movie 5', year: 2023 },
-      { title: 'Movie 6', year: 2023 },
-    ];
+// function loadMoreMovies() {
+//   if (!moviesLoaded) {
+//     // Symulacja ładowania filmów (w rzeczywistości tu może być API)
+//     const newMovies = [
+//       { title: 'Movie 4', year: 2023 },
+//       { title: 'Movie 5', year: 2023 },
+//       { title: 'Movie 6', year: 2023 },
+//     ];
 
-    const movieList = document.getElementById('movies');
+//     const movieList = document.getElementById('movies');
 
-    newMovies.forEach(movie => {
-      const movieCard = document.createElement('div');
-      movieCard.classList.add('movie-card');
-      movieCard.innerHTML = `
-        <h3>${movie.title}</h3>
-        <p>${movie.year}</p>
-      `;
-      movieList.insertBefore(movieCard, document.getElementById('load more')); // Wstaw przed przyciskiem
-    });
+//     newMovies.forEach(movie => {
+//       const movieCard = document.createElement('div');
+//       movieCard.classList.add('movie-card');
+//       movieCard.innerHTML = `
+//         <h3>${movie.title}</h3>
+//         <p>${movie.year}</p>
+//       `;
+//       movieList.insertBefore(movieCard, document.getElementById('load more')); // Wstaw przed przyciskiem
+//     });
 
-    // Zaznacz, że załadowano filmy, aby zapobiec wielokrotnemu ładowaniu
-    moviesLoaded = true;
+//     // Zaznacz, że załadowano filmy, aby zapobiec wielokrotnemu ładowaniu
+//     moviesLoaded = true;
 
-    // Możesz ukryć przycisk See All, jeśli nie chcesz go pokazywać po załadowaniu
-    document.getElementById('load more').style.display = 'none';
-  }
-}
+//     // Możesz ukryć przycisk See All, jeśli nie chcesz go pokazywać po załadowaniu
+//     document.getElementById('load more').style.display = 'none';
+//   }
+// }
 // Pobranie elementów HTML
-const modal = document.getElementById('modal');
-const closeModalBtn = document.getElementById('closeModalBtn');
-const moviePoster = document.getElementById('moviePoster');
-const movieTitle = document.querySelector('.FirstModalTitle');
-const movieRating = document.getElementById('movieRating');
-const moviePopularity = document.querySelector('.FirstModalPopularity');
-const movieGenres = document.querySelector('.FirstModalGenres');
-const movieDescription = document.getElementById('movieDescription');
-const toggleLibraryBtn = document.getElementById('toggleLibraryBtn');
+// const modal = document.getElementById('modal');
+// const closeModalBtn = document.getElementById('closeModalBtn');
+// const moviePoster = document.getElementById('moviePoster');
+// const movieTitle = document.querySelector('.FirstModalTitle');
+// const movieRating = document.getElementById('movieRating');
+// const moviePopularity = document.querySelector('.FirstModalPopularity');
+// const movieGenres = document.querySelector('.FirstModalGenres');
+// const movieDescription = document.getElementById('movieDescription');
+// const toggleLibraryBtn = document.getElementById('toggleLibraryBtn');
 
 // const trailerContainer = document.getElementById('trailerContainer');
 // const watchTrailerBtn = document.getElementById('watchTrailerBtn');
 
 // Dodaj event listener do każdej karty
-document.querySelectorAll('movie-card').forEach(card => {
-  card.addEventListener('click', function (event) {
-    event.preventDefault(); // Zapobiega przeładowaniu strony
+// document.querySelectorAll('movie-card').forEach(card => {
+//   card.addEventListener('click', function (event) {
+//     // event.preventDefault(); // Zapobiega przeładowaniu strony
 
-    // Pobieranie szczegółów filmu z atrybutów data
-    const title = this.dataset.title;
-    const rating = this.dataset.rating;
-    const popularity = this.dataset.popularity;
-    const description = this.dataset.description;
-    const poster = this.dataset.poster;
+//     // // Pobieranie szczegółów filmu z atrybutów data
+//     // const title = this.dataset.title;
+//     // const rating = this.dataset.rating;
+//     // const popularity = this.dataset.popularity;
+//     // const description = this.dataset.description;
+//     // const poster = this.dataset.poster;
 
-    // Ustawianie zawartości modalu
-    document.getElementById('movieTitle').innerText = title;
-    document.getElementById('movieRating').innerText = `Rating: ${rating}`;
-    document.getElementById(
-      'moviePopularity'
-    ).innerText = `Popularity: ${popularity}`;
-    document.getElementById('movieDescription').innerText = description;
-    document.getElementById('moviePoster').src = poster;
+//     // // Ustawianie zawartości modalu
+//     // document.getElementById('movieTitle').innerText = title;
+//     // document.getElementById('movieRating').innerText = `Rating: ${rating}`;
+//     // document.getElementById(
+//     //   'moviePopularity'
+//     // ).innerText = `Popularity: ${popularity}`;
+//     // document.getElementById('movieDescription').innerText = description;
+//     // document.getElementById('moviePoster').src = poster;
 
-    // Wyświetlanie modalu
-    modal.classList.remove('hidden');
-  });
-});
+//     // // Wyświetlanie modalu
+//     // modal.classList.remove('hidden');
+//   });
+// });
 
 // watchTrailerBtn.addEventListener('click', function () {
 //   trailerContainer.classList.toggle('hidden');
@@ -178,45 +179,45 @@ document.querySelectorAll('movie-card').forEach(card => {
 // });
 
 // Obsługa zamknięcia modalu
-closeModalBtn.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  // trailerContainer.classList.add('hidden');
-  // trailerContainer.innerHTML = ''; // Usuwa zawartość trailera po zamknięciu
-});
+// closeModalBtn.addEventListener('click', function () {
+//   modal.classList.add('hidden');
+//   // trailerContainer.classList.add('hidden');
+//   // trailerContainer.innerHTML = ''; // Usuwa zawartość trailera po zamknięciu
+// });
 
-// Obsługa dodania filmu do biblioteki
-toggleLibraryBtn.addEventListener('click', function () {
-  // Przykładowe działanie: wyświetlenie alertu
-  alert('Film dodany do biblioteki!');
-  // Możesz dodać tutaj dodatkową logikę, np. przechowywanie danych w localStorage
-});
+// // Obsługa dodania filmu do biblioteki
+// toggleLibraryBtn.addEventListener('click', function () {
+//   // Przykładowe działanie: wyświetlenie alertu
+//   alert('Film dodany do biblioteki!');
+//   // Możesz dodać tutaj dodatkową logikę, np. przechowywanie danych w localStorage
+// });
 
 // Funkcja do wyświetlania modala
-export async function showModal(movie) {
-  currentMovie = movie;
-  moviePoster.src = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
-  movieTitle.innerText = movie.title;
-  movieRating.innerText = `${movie.vote_average.toFixed(1)} / ${movie.vote_count.toFixed(1)}`;
-  moviePopularity.innerText = `${movie.popularity.toFixed(1)}`;
-  const genreNames = getGenreNames(movie.genre_ids);
-  movieGenres.innerText = `${genreNames}`;
-  movieDescription.innerText = movie.overview;
+// export async function showModal(movie) {
+//   currentMovie = movie;
+//   moviePoster.src = `https://image.tmdb.org/t/p/original/${movie.poster_path}`;
+//   movieTitle.innerText = movie.title;
+//   movieRating.innerText = `${movie.vote_average.toFixed(1)} / ${movie.vote_count.toFixed(1)}`;
+//   moviePopularity.innerText = `${movie.popularity.toFixed(1)}`;
+//   const genreNames = getGenreNames(movie.genre_ids);
+//   movieGenres.innerText = `${genreNames}`;
+//   movieDescription.innerText = movie.overview;
 
-  updateLibraryButton();
-  modal.classList.remove('hidden');
+//   updateLibraryButton();
+//   modal.classList.remove('hidden');
 
-  document.addEventListener('keydown', handleEscKeyPress);
-}
+//   document.addEventListener('keydown', handleEscKeyPress);
+// }
 
-// Funkcja do zamykania modala
-function closeModal() {
-  modal.classList.add('hidden');
-  // trailerContainer.classList.add('hidden');
-  // trailerContainer.innerHTML = ''; // Usuń trailer z kontenera
-  document.removeEventListener('keydown', handleEscKeyPress);
-}
+// // Funkcja do zamykania modala
+// function closeModal() {
+//   modal.classList.add('hidden');
+//   // trailerContainer.classList.add('hidden');
+//   // trailerContainer.innerHTML = ''; // Usuń trailer z kontenera
+//   document.removeEventListener('keydown', handleEscKeyPress);
+// }
 
-// Funkcja obsługująca naciśnięcie klawisza ESC
-function handleEscKeyPress(event) {
-  if (event.key === 'Escape') closeModal();
-}
+// // Funkcja obsługująca naciśnięcie klawisza ESC
+// function handleEscKeyPress(event) {
+//   if (event.key === 'Escape') closeModal();
+// }
